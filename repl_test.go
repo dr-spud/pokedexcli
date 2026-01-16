4336c07de619af06f55f807c56cc21f182d2b049
dr-spud <xzhuc42@gmail.com>
2026-01-16 16:01:38 +1100

    repl

---
+++ repl_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input string
+		expected []string
+	}{
+		{
+			input: "hello world",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "this is a string",
+			expected: []string{"this", "is", "a", "string"},
+		},
+		{
+			input: "CAPITAL And lowercase",
+			expected: []string{"capital", "and", "lowercase"},
+		},
+		{
+			input: "   whitespace before and after string   ",
+			expected: []string{"whitespace", "before", "and", "after", "string"},
+		},
+		{
+			input: "big           space",
+			expected: []string{"big", "space"},
+		},
+	}
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("expected: %v, got: %v", c.expected, actual)
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord	{
+				t.Errorf("expected: %v, got %v", expectedWord, word)
+			}
+		}
+	}
+}
